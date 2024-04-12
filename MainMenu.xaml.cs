2c282ae2385b1cfe39e99947deeebcb55616523e
001192985 <Disney.LOKUGE@student.tafesa.edu.au>
2024-04-12 19:44:06 +0930

    Commit the mortgage calculation codes changes.

--- MainMenu.xaml.cs
+++ MainMenu.xaml.cs
@@ -26,5 +26,10 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
+
+		private void mortgageCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(MortgageCalculator));
+		}
 	}
 }
